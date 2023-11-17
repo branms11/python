b93290ba2bd39e6e327d8a235a8a48cbf800d817
Brandon Martins <brandon-martins@outlook.com>
2023-11-17 14:23:23 -0300

    teste

---
+++ aula22.py
@@ -0,0 +1,9 @@
+numero_de_atrasos = 2
+if numero_de_atrasos >= 3:
+  print('voce está suspenso')
+elif numero_de_atrasos == 1:
+  print('Pode entrar, porem mais 2 ramelada é F')
+elif numero_de_atrasos == 2:
+  print('entra ai mas ta no bico do corvo em')
+else:
+  print('Pode entrar')
